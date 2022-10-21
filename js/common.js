eb76be4b0c18120099a530086e1b9508bd16d2b2
‘xwl’ <1531119008@qq.com>
2022-10-21 17:31:05 +0800

    Site updated: 2022-10-21 17:31:05

--- js/common.js
+++ js/common.js
@@ -13,31 +13,3 @@ if (mobile_sidebar_menus) {
         menus_item_child[i].style.display = "none";
     }
 }
-
-
-
-
-
-//debug调试时跳转页面
-var element = new Image();
-Object.defineProperty(element,'id',{get:function(){window.location.href="https://www.baidu.com"}});
-console.log(element);
-
-
-setInterval(function() {
-        check();
-      }, 2000);
-      var check = function() {
-        function doCheck(a) {
-          if (('' + a / a)['length'] !== 1 || a % 20 === 0) {
-            (function() {}['constructor']('debugger')());
-          } else {
-            (function() {}['constructor']('debugger')());
-          }
-          doCheck(++a);
-        }
-        try {
-          doCheck(0);
-        } catch (err) {}
-      };
-      check();
